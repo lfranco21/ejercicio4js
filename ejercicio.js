9bfc5db254ac4d55a368d72616cb0b64a036ffb0
Luis Franco <lfranco21@users.noreply.github.com>
2023-01-08 16:15:15 -0500

    firts commit

---
+++ ejercicio.js
@@ -0,0 +1,20 @@
+let nombre = "Luis";
+let apellido = "Franco";
+let estudiante = `${nombre} ${apellido}`;
+
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus);
+
+let numero_caracteres = estudiante.length;
+console.log(numero_caracteres);
+
+let primera_letra = nombre.charAt(0);
+console.log(primera_letra);
+
+let ultima_letra = apellido.charAt(apellido.length-1)
+console.log(ultima_letra);
